38c6a40d1dd0e380d37499fbc1ab6bd902f512c8
Максим Казанцев <maks.aaron@gmail.com>
2018-05-29 10:38:55 +0400

    add Fixture

--- php4dvd/test_kazantsev.py
+++ php4dvd/test_kazantsev.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.common.exceptions import *
-import unittest
+from selenium_fixture import driver
+
+
+def test_login(driver):
+    driver.get("http://localhost/php4dvd/")
+    driver.find_element_by_id("username").clear()
+    driver.find_element_by_id("username").send_keys("admin")
+    driver.find_element_by_name("password").clear()
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("submit").click()
+    driver.find_element_by_link_text("Log out").click()
+    driver.switch_to_alert().accept()
+
+
+
+
 
 class Untitled(unittest.TestCase):
     def setUp(self):
